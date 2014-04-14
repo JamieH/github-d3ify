289185ded1741ef25006a65d8483d5ae81c18d33
Jamie Hankins <jamiehankin@gmail.com>
2014-04-14 10:03:00 +0100

    Add json.net and some C# classes for the .json files

---
+++ D3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github_d3ify
+{
+    class D3
+    {
+        public class Project
+        {
+            public string name { get; set; }
+            public int size { get; set; }
+        }
+
+        public class Language
+        {
+            public string name { get; set; }
+            public List<Project> children { get; set; }
+        }
+
+        public class Repository
+        {
+            public string name { get; set; }
+            public List<Language> children { get; set; }
+        }
+    }
+}
