478d02b187eb6f2dbb4a64cec6eb08a15cd9e22c
Jamie Hankins <jamiehankin@gmail.com>
2014-04-14 11:03:16 +0100

    reserialize the data

--- Program.cs
+++ Program.cs
@@ -16,8 +16,12 @@ namespace github_d3ify
             WebClient wc = new WebClient();
             wc.Headers.Add("User-Agent:  RepoDataDownloader - JamieH");
             string githubdata = wc.DownloadString("https://api.github.com/users/JamieH/repos");
-
-            getD3File(githubdata);
+            
+            var d3obj = getD3File(githubdata);
+            var d3json = JsonConvert.SerializeObject(d3obj);
+            
+            Console.WriteLine(d3json);
+            Console.ReadLine();
         }
 
         public static D3.Repository getD3File(string githubjson)
@@ -25,18 +29,52 @@ namespace github_d3ify
 
             dynamic github = JsonConvert.DeserializeObject<dynamic>(githubjson);
 
+            Dictionary<string, List<Github.Project>> languages = new Dictionary<string, List<Github.Project>>();
+
             foreach(dynamic s in github)
             {
-                Console.WriteLine("---");
-                Console.WriteLine(s.name);
-                Console.WriteLine(s.size);
-                Console.WriteLine(s.language);
-                Console.WriteLine("---");
+                if(!languages.ContainsKey(s.language.ToString()))
+                {
+                    var list = new List<Github.Project>();
+                    languages.Add(s.language.ToString(), list);
+                }
+                
+                
+                    Github.Project ghp = new Github.Project();
+                    ghp.name = s.name;
+                    ghp.size = s.size;
+                    ghp.language = s.language;
+                    languages[s.language.ToString()].Add(ghp);
+               
+                //Console.WriteLine("---");
+                //Console.WriteLine(s.name);
+                //Console.WriteLine(s.size);
+                //Console.WriteLine(s.language);
+                //Console.WriteLine("---");
             }
-            
-            D3.Repository d3 = new D3.Repository();
-            Console.ReadLine();
-            return d3;
+
+            D3.Repository repo = new D3.Repository();
+            repo.name = "flare";
+            repo.children = new List<D3.Language>();
+
+            foreach(string lang in languages.Keys)
+            {
+                D3.Language children = new D3.Language();
+                children.name = lang;
+                children.children = new List<D3.Project>();
+
+                foreach(Github.Project ghproj in languages[lang])
+                {
+                    D3.Project d3prog = new D3.Project();
+                    d3prog.name = ghproj.name;
+                    d3prog.size = ghproj.size;
+                    children.children.Add(d3prog);
+                }
+                Console.WriteLine("Language: {0} has {1} projects", lang, children.children.Count);
+                repo.children.Add(children);
+            }
+
+            return repo;
         }
     }
 }
